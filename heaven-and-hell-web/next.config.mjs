66f749b4463c82b585941774704a2b613746c069
Yan Brandão Tapajós <ybrandao.d@gmail.com>
2026-04-17 13:51:11 +0100

    feat: update components,next.config.mjs in heaven-and-hell-web

--- heaven-and-hell-web/next.config.mjs
+++ heaven-and-hell-web/next.config.mjs
@@ -3,6 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin"
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts")
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "picsum.photos" },
+    ],
+  },
+}
 
 export default withNextIntl(nextConfig)
